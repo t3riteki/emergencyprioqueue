8a10eac59235aed720de6e5f0061ee4823ae7a53
t3riteki <gobythebox@gmail.com>
2024-12-27 02:19:11 +0800

    Revert "Im not sure"
    This reverts commit 85910877fd7922cd4b49782e011a2840cef4d130.

--- src/main/java/datastruct/le/App.java
+++ src/main/java/datastruct/le/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("TaskviewUI"));
+        scene = new Scene(loadFXML("Taskview_UI"));
         stage.setScene(scene);
         stage.show();
     }
