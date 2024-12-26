7f0f37e91dc98d595d2d6ee09769a67f49a968d2
t3riteki <gobythebox@gmail.com>
2024-12-27 02:44:20 +0800

    Dir Changes
    Changes in directory

--- src/main/java/datastruct/le/App.java
+++ src/main/java/datastruct/le/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Taskview_UI"));
+        scene = new Scene(loadFXML("TaskviewUI"));
         stage.setScene(scene);
         stage.show();
     }
